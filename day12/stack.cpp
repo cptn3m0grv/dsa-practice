c6f8479eeecb332738ee425ac939ceca6cdbdb80
Gaurav Goyal <cptn3m0grv@gmail.com>
2020-09-11 13:31:58 +0530

    stack

--- day12/stack.cpp
+++ day12/stack.cpp
@@ -23,9 +23,17 @@ bool isFull(st *s){
     }
 }
 
+bool isEmpty(st *s){
+    if(s->top == -1){
+        return true;
+    }else{
+        return false;
+    }
+}
+
 void push(st *s, int data){
     if(isFull(s)){
-
+        cout << "The stack is already full, please pop an element." << endl;
     }else{
         s->top ++;
         s->item[s->top] = data;
@@ -33,21 +41,45 @@ void push(st *s, int data){
     }
 }
 
+void pop(st *s){
+    if(isEmpty(s)){
+        cout << "The stack is already empty." << endl;
+    }else{
+        s->top--;
+        count --;
+    }
+}
+
 void printStack(st *s){
     int i;
-    cout << "Stack: ";
+    cout << "Stack: \n";
     for(i=0; i<count; i++){
         cout<< s->item[i] << endl;
     }
 }
 
 int main(){
+
     st *s = (st *)malloc(sizeof(st));
 
     createEmptyStack(s);
 
     push(s, 2);
-    push(s, 5);
+    push(s, 343);
+    push(s, 3234);
+    push(s, 434);
+    push(s, 43545);
+    push(s, 4654);
+    push(s, 43546);
+    push(s, 54654);
+    push(s, 4);
+
+    printStack(s);
+
+    pop(s);
+    pop(s);
+    pop(s);
+    pop(s);
 
     printStack(s);
 
