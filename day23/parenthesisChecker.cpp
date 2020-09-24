3fcd505c6367554925c9f4721830f7cfd852292f
Gaurav Goyal <cptn3m0grv@gmail.com>
2020-09-25 00:08:23 +0530

    paranthesis checker

--- day23/parenthesisChecker.cpp
+++ day23/parenthesisChecker.cpp
@@ -1,73 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct Stack{
-    int size;
-    int top;
-    char *arr;
-};
-
-bool isEmpty(struct Stack * ptr){
-    return (ptr->top == -1 ? true : false);
-}
-
-bool isFull(struct Stack * ptr){
-    return (ptr->top == ptr->size-1 ? true : false);
-}
-
-void push(struct Stack * ptr, char val){
-    if(isFull(ptr)){return;}
-    else{
-        ptr->top++;
-        ptr->arr[ptr->top] = val;
+void check(char c[]){
+    stack<char> s;
+    int balanced = 1;
+    for(int i = 0; c[i] != '\0'; i++){
+        if(c[i]=='(' || c[i]=='{' || c[i]=='['){
+            s.push(c[i]);
+        }else if(c[i]==')' || c[i]=='}' || c[i]==']'){
+            if(s.empty()==true){
+                balanced = 0;
+                break;
+            }else if(c[i]==')' && s.top()=='('){
+                s.pop();
+            }else if(c[i]=='}' && s.top()=='{'){
+                s.pop();
+            }else if(c[i]==']' && s.top()=='['){
+                s.pop();
+            }else{
+                balanced = 0;
+            }
+        }
     }
-}
-
-char pop(struct Stack * ptr){
-    if(isEmpty(ptr)) {return 'a';}
-    else{
-        char val = ptr->arr[ptr->top];
-        ptr->top--;
-        return val;
+    if(balanced && s.empty()){
+        printf("balanced\n");
+    }else{
+        printf("not balanced\n");
     }
+
 }
 
-int main(){
+int main(void){
 
     int test;
     scanf("%d", &test);
+
     while(test--){
-        int balanced;
-
-        char str[100];
+        char str[10000];
         scanf("%s", str);
-
-        struct Stack *sp = (struct Stack *)malloc(sizeof(struct Stack));
-        sp->size = 51;
-        sp->top = -1;
-        sp->arr = (char *)malloc(sp->size * sizeof(char));
-
-        for(int i = 0; i < 100; i++){
-            if(str[i] == '(' || str[i] == '[' || str[i] == '{'){
-                push(sp, str[i]);
-            }else if(str[i] == ')'){
-                if(pop(sp) == '(') balanced == 1;
-                else{balanced = 0;}
-            }else if(str[i] == '}'){
-                if(pop(sp) == '{') balanced == 1;
-                else{balanced = 0;}
-            }else if(str[i] == ']'){
-                if(pop(sp) == '[') balanced == 1;
-                else{balanced = 0;}
-            }
-        }
-        if(balanced == 0){
-            printf("Balanced.\n");
-        }else{
-            printf("Not Balanced.\n");
-        }
+        check(str);
     }
-
 
     return 0;
 }
