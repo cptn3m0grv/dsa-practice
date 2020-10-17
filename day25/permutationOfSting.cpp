170607f2790b9993e251ef9061a2e876fa64acda
Gaurav Goyal <cptn3m0grv@gmail.com>
2020-10-17 13:08:17 +0530

    permutation of String fixed

--- day25/permutationOfSting.cpp
+++ day25/permutationOfSting.cpp
@@ -1,27 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void permute(string str, int left, int right){
-    if(left == right){
-        cout << str << " ";
-    }else{
-        for(int i = left; i <= right; i++){
-            swap(str[left], str[i]);
-            permute(str, left+1, right);
-            swap(str[left], str[i]);
-        }
-    }
-}
+#define FASTIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
 
 int main(){
-    int test;
-    scanf("%d", &test);
-    while(test--){
-        string str;
-        cin >> str;
-        int size = str.size();
-        permute(str, 0, size-1);
-        printf("\n");
-    }
+    FASTIO;
+    int t;
+    cin >> t;
+    while(t--){
+        string s;
+        cin >> s;
+        sort(s.begin(), s.end());
+        do{
+            cout << s << " ";
+        }while(next_permutation(s.begin(), s.end()));
+        cout << endl;
+    }   
     return 0;
 }
