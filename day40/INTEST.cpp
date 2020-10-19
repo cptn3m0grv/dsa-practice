65131640a866c21f724be5e61bbd109c69247a8f
Gaurav Goyal <cptn3m0grv@gmail.com>
2020-10-20 00:39:00 +0530

    simple codechef problem

---
+++ day40/INTEST.cpp
@@ -0,0 +1,40 @@
+// The purpose of this problem is to verify whether the method you are using to read input data is sufficiently fast to handle problems branded with the enormous Input/Output warning. You are expected to be able to process at least 2.5MB of input data per second at runtime.
+
+// Input
+// The input begins with two positive integers n k (n, k<=107). The next n lines of input contain one positive integer ti, not greater than 109, each.
+
+// Output
+// Write a single integer to output, denoting how many integers ti are divisible by k.
+
+// Example
+// Input:
+// 7 3
+// 1
+// 51
+// 966369
+// 7
+// 9
+// 999996
+// 11
+
+// Output:
+// 4
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FASTIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+int main(){
+    FASTIO;
+    int n, k;
+    cin >> n >> k;
+    int tot = 0;
+    while(n--){
+        int tI;
+        cin >> tI;
+        if(tI % k == 0) tot += 1;
+    }
+    cout << tot;
+    return 0;
+}
