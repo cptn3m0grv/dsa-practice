30ad1fc4bfaf08a85e17b4804ec8c274d1f225eb
Gaurav Goyal <cptn3m0grv@gmail.com>
2020-11-11 12:59:59 +0530

    C++ template

---
+++ template.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define ll long long
+
+int main(){
+    fastio;
+    int t; cin>>t;
+    while(t-- > 0){
+        
+    }
+    return 0;
+}
