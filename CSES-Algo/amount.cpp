a7e0731de3185f5ae0707c931dd0600ef592e7eb
Gaurav Goyal <cptn3m0grv@gmail.com>
2020-11-15 09:20:22 +0530

    amount CSES Algorithms problem

---
+++ CSES-Algo/amount.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL)
+
+ll INF=1e18+10;
+
+int main(){
+    fastio;
+    ll n;
+    cin >> n;
+    cout << n*(n+1)/2;
+
+    return 0;
+}
