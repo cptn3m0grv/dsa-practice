770e4b7eb332eac041f874deb7591eab84537851
Gaurav Goyal <cptn3m0grv@gmail.com>
2020-11-19 00:04:29 +0530

    sum of numbers in a string gfg problem

---
+++ day61/sum-of-numbers-in-string.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+
+int main(){
+    fastio;
+    int t; cin>>t;
+    while(t-- > 0){
+        string s;
+        cin >> s;
+        int size = s.size();
+        int mXm = INT_MIN;
+        int sum = 0;
+        for(int i = 0; i < size; ++i){
+            string curr;
+            if(s[i]>='0'&&s[i]<='9'){
+                curr+=s[i];
+                while(i+1<size&&s[i]>='0'&&s[i]<='9'){
+                    curr+=s[i+1];
+                    ++i;
+                }
+            }
+            if(curr.size()!=0){
+                sum+=stoi(curr);
+            }
+        }
+        cout << sum << "\n";
+    }
+
+    return 0;
+}
