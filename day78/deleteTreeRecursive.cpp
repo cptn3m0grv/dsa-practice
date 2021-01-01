3f341d0dc42a05a43e1daac7276d874076169cd0
Gaurav Goyal <cptn3m0grv@gmail.com>
2021-01-01 10:58:37 +0530

    problems on binary trees

---
+++ day78/deleteTreeRecursive.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+
+struct Node{
+    int data;
+    Node *left, *right;
+};
+
+Node *newNode(int data){
+    Node *temp = new Node();
+    temp->data = data;
+    temp->left = temp->right = nullptr;
+    return temp;
+}
+
+
+void deleteTree(Node *root){
+    if(root==nullptr){
+        return;
+    }
+    
+    deleteTree(root->left);
+    deleteTree(root->right);
+    
+    delete root;
+    
+    root = nullptr;
+}
+
+
+int main(){
+    fastio;
+    
+    Node *root = newNode(15);
+    root->left = newNode(10);
+    root->right = newNode(2);
+    root->left->left = newNode(3);
+    root->left->right = newNode(4);
+    root->left->left->left = newNode(6);
+    root->right->right = newNode(5);
+    
+    deleteTree(root);
+    
+    
+    return 0;
+}
