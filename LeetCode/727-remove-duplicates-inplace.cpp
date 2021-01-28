c9ecd642f63d9485f7c00a3c4a39aa1a778aeae4
Gaurav Goyal <cptn3m0grv@gmail.com>
2021-01-28 09:29:59 +0530

    removind duplicate numbers in place in array

---
+++ LeetCode/727-remove-duplicates-inplace.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int size = nums.size();
+        if(size==0 || size==1){
+            return size;
+        }
+        
+        int j = 0;
+        for(int i = 0; i < size-1; ++i){
+            if(nums[i]!=nums[i+1]){
+                nums[j++] = nums[i];
+            }
+        }
+        nums[j++] = nums[size-1];
+        return j;
+                
+    }
+};
