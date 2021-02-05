bebe7867127fc8169c49d597bbf429db2c49fa55
Gaurav Goyal <cptn3m0grv@gmail.com>
2021-02-05 09:17:43 +0530

    push back 6 problem solved

---
+++ Codechef/push_back_6/ITGUY86.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define ll long long
+
+
+void solve(){
+        int N;
+        cin>>N;
+        
+        for(int i=1;i<=N;++i){
+            cout<<".";
+            for(int j=1;j<=i;++j){
+                cout<<i;
+            }
+            cout<<"\n";
+        }
+}
+
+int main(){
+    fastio;
+    int t; cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
