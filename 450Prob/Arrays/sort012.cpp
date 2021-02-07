24cd81c89c51675321b6212322c9bf2cea15be99
Gaurav Goyal <cptn3m0grv@gmail.com>
2021-02-07 10:31:49 +0530

    sorting array with 0, 1, 2

---
+++ 450Prob/Arrays/sort012.cpp
@@ -0,0 +1,54 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+void sort012(int[],int);
+
+int main() {
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >>n;
+        int a[n];
+        for(int i=0;i<n;i++){
+            cin >> a[i];
+        }
+
+        sort012(a, n);
+
+        for(int i=0;i<n;i++){
+            cout << a[i]  << " ";
+        }
+
+        cout << endl;
+        
+        
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+
+void sort012(int a[], int n)
+{
+    // coode here 
+    int low = 0;
+    int mid = 0;
+    int high = n-1;
+    
+    while(mid<=high){
+        if(a[mid]==0){
+            swap(a[low], a[mid]);
+            low++;
+            mid++;
+        }else if(a[mid]==1){
+            mid++;
+        }else if(a[mid]==2){
+            swap(a[mid], a[high]);
+            high--;
+        }
+    }
+}
