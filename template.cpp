95dd9952e27f45664b9a1ca0f192daa142ce54d0
Gaurav Goyal <cptn3m0grv@gmail.com>
2021-02-16 09:21:42 +0530

    changes in c++ template

--- template.cpp
+++ template.cpp
@@ -1,14 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define fastio ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define fastio ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
 #define ll long long
+
+
+void solve(){
+	
+
+	cout << "\n";
+}
 
 int main(){
     fastio;
     int t; cin>>t;
-    while(t-- > 0){
-        
+    while(t--){
+        solve();
     }
     return 0;
 }
