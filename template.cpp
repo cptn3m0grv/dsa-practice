ba49dc5b2add7e5db2ea2d7ba575c3b1a04e7804
Gaurav Goyal <cptn3m0grv@gmail.com>
2021-03-02 11:58:27 +0530

    execution time calculated

--- template.cpp
+++ template.cpp
@@ -13,9 +13,11 @@ void solve(){
 
 int main(){
     fastio;
+    clock_t tStart = clock();
     int t; cin>>t;
     while(t--){
         solve();
     }
+    printf("Time taken: %.2fs\n",(double)(clock()-tStart)/CLOCKS_PER_SEC );
     return 0;
 }
