630b08c03ae771e8cc8f35cf26eba66bab04ac28
Gaurav Goyal <cptn3m0grv@gmail.com>
2021-03-10 13:52:17 +0530

    Cloning Graph Problem

---
+++ LeetCode/Graphs/133-Clone-Graph.cpp
@@ -0,0 +1,46 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    void dfs(Node *node, Node *copy, vector<Node*>& vis){
+        vis[copy->val] = copy;
+        for(auto x: node->neighbors){
+            if(vis[x->val]==NULL){
+                Node *temp = new Node(x->val);
+                (copy->neighbors).push_back(temp);
+                dfs(x, temp, vis);
+            }else{
+                (copy->neighbors).push_back(vis[x->val]);
+            }
+        }
+    }
+    
+    Node* cloneGraph(Node* node) {
+        if(node==NULL){
+            return NULL;
+        }
+        Node *copy = new Node(node->val);
+        vector<Node*> vis(105, NULL);
+        dfs(node, copy, vis);
+        return copy;
+    }
+};
